5799f998fed3432e69c3e1934b158daf0027b27d
HausCloud <696@holbertonschool.com>
2019-09-30 20:14:46 +0000

    create script that updates and refreshes main page after clicking the search button with boxes selected

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,93 @@
+#!/usr/bin/node
+
+let myObj = {}
+$(function () {
+  $('input:checkbox').change(
+    function () {
+      if ($(this).is(':checked')) {
+	myObj[($(this).attr("data-name"))] = $(this).attr("data-id")
+      } else {
+	delete myObj[$(this).attr("data-name")]
+      }
+      $('.amenitiesandbeyond').empty()
+      if (Object.keys(myObj).length === 1) {
+	for (const key in myObj) {
+          $('.amenitiesandbeyond').append(key)
+	}
+      } else {
+	for (const key in myObj) {
+	  $('.amenitiesandbeyond').append(key + ', ')
+	}
+      }
+    })
+
+  $.ajax({
+    type: "POST",
+    url: "http://0.0.0.0:5001/api/v1/places_search",
+    contentType: 'application/json',
+    data: '{}',
+    success: loadPage
+  })
+
+  $('button').click( function() {
+    if (Object.keys(myObj).length === 0) {
+      $.ajax({
+	type: "POST",
+	url: "http://0.0.0.0:5001/api/v1/places_search",
+	contentType: 'application/json',
+	data: '{}',
+	success: loadPage
+      })
+    } else {
+      $.ajax({
+	type: "POST",
+	url: "http://0.0.0.0:5001/api/v1/places_search",
+	contentType: 'application/json',
+	data: JSON.stringify({'amenities': Object.values(myObj)}),
+	success: function (data) {
+	  $("section.places").html("<h1>Places</h1>");
+	  loadPage (data);
+	},
+	error: function (error) {
+    	  console.log('failure!')
+	}
+      })
+    }
+  })
+});
+
+function loadPage (data) {
+  for (const place in data) {
+    const articleTag = `
+	  <article>
+	  <div class="title">
+	  <h2>${data[place].name}</h2>
+	  <div class="price_by_night">
+	  ${data[place].price_by_night}
+    </div>
+      </div>
+      <div class="information">
+      <div class="max_guest">
+      <i class="fa fa-users fa-3x" aria-hidden="true"></i>
+      <br />
+      ${data[place].max_guest} Guests
+    </div>
+      <div class="number_rooms">
+      <i class="fa fa-bed fa-3x" aria-hidden="true"></i>
+      <br />
+      ${data[place].number_rooms} Bedrooms
+    </div>
+      <div class="number_bathrooms">
+      <i class="fa fa-bath fa-3x" aria-hidden="true"></i>
+      <br />
+      ${data[place].number_bathrooms} Bathroom
+    </div>
+      </div>
+      <div class="description">
+      ${data[place].description}
+    </div>
+      </article>
+      `;
+    $("section.places").append(articleTag)
+  }
+}
